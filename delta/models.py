97643c0448f32e891cf3d9743ac485d540e276a6
itsrichardyu <deadlyhunter295@gmail.com>
2023-01-14 13:00:21 -0800

    Redesigned Django models

--- delta/models.py
+++ delta/models.py
@@ -2,22 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Rating(models.Model):
-    user = models.ManyToManyField(User, null=True, blank=False)
-    when = models.DateTimeField(auto_now=True)
-    rating = models.IntegerField()
+class DiningLocation(models.Model):
+    location = models.CharField(max_length=18)
+    
+class MealPeriod(models.Model):
+    location = models.ForeignKey(DiningLocation, on_delete=models.CASCADE, null=True, blank=False)
+    period = models.CharField(max_length=18)
+    date = models.DateField()
 
-class FoodItem(models.Model):
+class MenuItem(models.Model):
+    meal_period = models.ForeignKey(MealPeriod, on_delete=models.CASCADE, null=True, blank=False)
     uid = models.CharField(max_length=64)
     name = models.CharField(max_length=128)
     category = models.CharField(max_length=64)
-    rating = models.ForeignKey(Rating, on_delete=models.CASCADE, null=True, blank=False)
 
-class MealPeriod(models.Model):
-    period = models.CharField(max_length=18)
-    date = models.DateField()
-    items_offered = models.ForeignKey(FoodItem, on_delete=models.CASCADE, null=True, blank=False)
+class Rating(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, null=True, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=False)
+    rating = models.IntegerField()
+    when = models.DateTimeField()
 
-class DiningLocation(models.Model):
-    location = models.CharField(max_length=18)
-    meal_period = models.ForeignKey(MealPeriod, on_delete=models.CASCADE, null=True, blank=False)
+# TODO: write string representations of models
