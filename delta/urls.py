2d6c95d4f33a81ffbc26e4adedb355233be44548
Richard Yu <richardyu@berkeley.edu>
2023-01-16 21:54:12 -0800

    Improved application frontend

--- delta/urls.py
+++ delta/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     # e.g. /dashboard/crossroads
     path('<str:location>/', views.dining_location, name='dining_location'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
